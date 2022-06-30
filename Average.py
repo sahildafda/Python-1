0738facf511344a24bd7b0bfcb49f5257e7e3bbd
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-06-30 10:09:45 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,6 @@
+#To find average of any two numbers.
+
+number1 = float(input("Enter first average number:-"))
+number2 = float(input("Enter secound average number:-"))
+avg = (number1+number2)/2
+print("total number of average",avg)
