0a70dd1a0a6f258df8944c77327aff437c3b242c
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-06-30 10:30:58 +0530

    Add files via upload

---
+++ Simple_Interest.py
@@ -0,0 +1,8 @@
+#To find simple interest. Simple Interest = p * r * n / 100. Where p = principle amount, r = rate of interest and n=number of years
+
+p = float(input("Enter principal amount :- " ))
+r = float(input("Enter rate of interest :- "))
+n = float(input("Enter number of year :- "))
+interst = p*r*n/100
+print(interst)
+
